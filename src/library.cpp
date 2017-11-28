4d732519b9638676a38972187bccb4c40d83076e
Unknown <leonardo.feltran@gmail.com>
2017-11-28 13:20:36 -0200

    Add commens at insertSong method

--- src/library.cpp
+++ src/library.cpp
@@ -17,14 +17,17 @@ void Library::generateIndex(){
 
 //insert the song in the btree and in the datafile
 void Library::insertSong(Song* song){
-
+	//Creates a filestram
 	fstream dFile;
+	//Opens the file in append mode
 	dFile.open(dataFile.data(), fstream::app);
-
-	int size = song->integerDigits(song->getId()) + song->getTitle().length() + song->getGenre().length() + 4;
-
-	dFile << size << '|' << song->getId() << '|' << song->getTitle() << '|' << song->getGenre() << '|';
-
+	//Calculates the char amount
+	int size = song.integerDigits(song.getId()) + song.getTitle().length() + song.getGenre().length() + 4;
+	//Writes on file
+	dFile << size << '|' << song.getId() << '|' << song.getTitle() << '|' << song.getGenre() << '|';
+	//Close the file
+	dFile.close();
+	
 }
 
 Song* Library::searchSong(key_t id){
