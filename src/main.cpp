f0df38342ab41cfd80f16bcda7d1db8e010e7ac0
Unknown <cesaralbc@gmail.com>
2017-12-05 12:43:30 -0200

    Reorganize the menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,184 +1,193 @@
 #include <iostream>
 #include <istream>
-//#include <windows.h>
-//#include <conio.h>
+#include <windows.h>
+#include <conio.h>
 #include "library.hpp"
 
 using namespace std;
 
-//Change the statements' color
-void TrocaCor(int x){
-    HANDLE  hConsole;
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, x);
-}
+// function prototypes
+void changeColor(int color);
+void printMenu(int menu);
+void menuAction(int menu, Library lib);
 
+// main is responsible for receiving user input and calling library methods
+int main(int argc, char *argv[]){
 
-//menu management function
-void GerenciaMenu(int menu){
-    system("cls");
-    //manages the menu from the menu variable
-    switch(menu){
-        //Create index
-        case 1:
-            cout<<endl<<"Funcao de Criar Indice, aperte ENTER para prosseguir.";
-            getchar();
-        break;
-        //Insert Music
-        case 2:
-            cout<<endl<<"Funcao de Inserir Musica, aperte ENTER para prosseguir.";
-            getchar();
-        break;
-        //Search Music
-        case 3:
-            cout<<endl<<"Funcao de Procurar uma Musica, aperte ENTER para prosseguir.";
-            getchar();
-        break;
-        //Remove Music
-        case 4:
-            cout<<endl<<"Funcao de Remover uma Musica, aperte ENTER para prosseguir.";
-            getchar();
-        break;
-        //Show B tree
-        case 5:
-            cout<<endl<<"Funcao para Mostrar a B-tree, aperte ENTER para prosseguir.";
-            getchar();
-        break;
+    // Library managing file operations
+    Library lib;
+
+    // menu variables
+    char c = 0;
+    int menuOption = 1;
+
+    // beginning of menu loop
+    while (c != -1){
+
+        // clear screen and print title
+        system("cls");
+        changeColor(207);
+        cout<<"Menu B-Tree:";
+        changeColor(15);
+
+        // print options
+        printMenu(menuOption);
+
+        // read a char and proceed accordingly
+        c = getch();
+
+        // if it was a number key
+        if (c >= 49 && c <= 54)
+            menuOption = c - 48;
+
+        // if it was arrow down
+        else if (c == 80) {
+            if(menuOption != 6)
+                menuOption++;
+            //If it is in the border, jump to the other side
+            else
+                menuOption = 1;
+        }
+
+        // if it was arrow up
+        else if (c == 72) {
+            if(menuOption != 1)
+                menuOption--;
+            //If it is in the border, jump to the other side
+            else
+                menuOption = 6;
+        }
+
+        // if it was ENTER key
+        else if (c == 13) {
+            // stop loop if it is option 6
+            if (menuOption == 6)
+                c = -1;
+            else
+                menuAction(menuOption, lib);
+        }
     }
 
+    // print finalization message
+    changeColor(207);
+    cout<<endl<<"Programa finalizado!";
+    changeColor(15);
 
-
+    return 0;
 }
 
-//Print Menu Function
-void PrintaMenu(int menu){
+// change printing color
+void changeColor(int color){
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+// print the menu options with the correct colors and highlights
+void printMenu(int menu){
     //Index
     if(menu==1){
         cout << endl << "------->";
-        TrocaCor(240);
+        changeColor(240);
         cout << "1. Criar Indice";
-        TrocaCor(15);
-    }else{
+        changeColor(15);
+    } else
         cout<<endl<<"\t1. Criar Indice";
-    }
+
     //Insert Music
     if(menu==2){
         cout<<endl<<"------->";
-        TrocaCor(240);
+        changeColor(240);
         cout<<"2. Inserir Musica";
-        TrocaCor(15);
-    }else{
+        changeColor(15);
+    } else
         cout<<endl<<"\t2. Inserir Musica";
-    }
+
     // Search Music
     if(menu==3){
         cout<<endl<<"------->";
-        TrocaCor(240);
+        changeColor(240);
         cout<<"3. Pesquisar Musica por ID";
-        TrocaCor(15);
-    }else{
+        changeColor(15);
+    } else
         cout<<endl<<"\t3. Pesquisar Musica por ID";
-    }
+
     // Remove Music
     if(menu==4){
         cout<<endl<<"------->";
-        TrocaCor(240);
+        changeColor(240);
         cout<<"4. Remover Musica por ID";
-        TrocaCor(15);
-    }else{
+        changeColor(15);
+    } else
         cout<<endl<<"\t4. Remover Musica por ID";
-    }
+
     // Print B tree
     if(menu==5){
         cout<<endl<<"------->";
-        TrocaCor(240);
+        changeColor(240);
         cout<<"5. Mostrar Arvore";
-        TrocaCor(15);
-    }else{
+        changeColor(15);
+    } else
         cout<<endl<<"\t5. Mostrar Arvore";
-    }
 
     //Close the program
     if(menu==6){
         cout<<endl<<"------->";
-        TrocaCor(240);
+        changeColor(240);
         cout<<"6. Fechar o Programa";
-        TrocaCor(15);
-    }else{
+        changeColor(15);
+    } else
         cout<<endl<<"\t6. Fechar o Programa";
-    }
-
-
 }
 
+// receive the selected menu option and behave accordingly
+void menuAction(int option, Library lib){
+    // clear screen
+    system("cls");
 
-int main(int argc, char *argv[]){
-
-    //menu
-    Library lib;
-    int c,menu=1,TAM=6;
-
-    while (c!=27){
-
-        system("cls");
-        TrocaCor(207);
-        cout<<"Menu B-Tree:";
-        TrocaCor(15);
-
-        PrintaMenu(menu);
-
-        c = getch();
-            switch(c){
-                //case 1
-            case 49:
-                menu = 1;
-            break;
-                //case 2
-            case 50:
-                menu = 2;
-            break;
-                //case 3
-            case 51:
-                menu = 3;
-            break;
-                //case 4
-            case 52:
-                menu = 4;
-            break;
-                //case 5
-            case 53:
-                menu = 5;
-            break;
-                //case 6
-            case 54:
-                menu = 6;
-            break;
-            //Down arrow
-            case 80:
-                if(menu!=TAM) menu++;
-                //If it is in the border, jump to the other side
-                else menu = 1;
+    // decides what to do
+    switch(option){
+        //Create index
+        case 1:
+            cout<<endl<<"Funcao de Criar Indice, aperte ENTER para prosseguir.";
+            getchar();
             break;
 
-            //Up arrow
-            case 72:
+        //Insert Music
+        case 2: {
+            cout << endl << "Funcao de Inserir musica, insira as informacoes abaixo.\n";
+            Song newSong;
 
-                if(menu!=1) menu--;
-                //If it is in the border, jump to the other side
-                else menu = 6;
+            cout << "\nId da musica:\n";
+            cin >> newSong.id;
+            cin.ignore(1000, '\n');
+
+            cout << "\nTitulo da musica:\n";
+            getline(cin, newSong.title);
+
+            cout << "\nGenero da musica:\n";
+            getline(cin, newSong.genre);
+
+            lib.insertSong(newSong);
+            cout << "\nMusica inserida com sucesso.\nAperte ENTER para prosseguir.";
+            getchar();
+
             break;
-
-            //Enter
-            case 13:
-                //Close program if menu it is the case 6
-                if (menu==TAM) c=27;
-                else GerenciaMenu(menu);
+        }
+        //Search Music
+        case 3:
+            cout<<endl<<"Funcao de Procurar uma Musica, aperte ENTER para prosseguir.";
+            getchar();
             break;
-
-            }
+        //Remove Music
+        case 4:
+            cout<<endl<<"Funcao de Remover uma Musica, aperte ENTER para prosseguir.";
+            getchar();
+            break;
+        //Show B tree
+        case 5:
+            cout<<endl<<"Funcao para Mostrar a B-tree, aperte ENTER para prosseguir.";
+            getchar();
+            break;
     }
-    TrocaCor(207);
-    cout<<endl<<"Programa finalizado!";
-    TrocaCor(15);
-    return 0;
 }
